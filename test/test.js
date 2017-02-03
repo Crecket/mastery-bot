c84169bd5fa28684d5607cd68b0fd0f0b93fcaef
crecket <crecketgaming@gmail.com>
2017-02-03 14:47:46 +0100

    summoner profile test

--- test/test.js
+++ test/test.js
@@ -2,10 +2,12 @@ const assert = require('assert');
 const fs = require('fs');
 const Parser = require('../src/Parser');
 const ChampionHighscoresResponseTemplate = require('../src/ResponseTemplates/ChampionHighscores');
+const SummonerResponseTemplate = require('../src/ResponseTemplates/Summoner');
 
 // sample data
 const championHighscoreData = JSON.parse(fs.readFileSync('./test/sampleData/championHighscoreData.json'));
 const championTestData = JSON.parse(fs.readFileSync('./test/sampleData/championTestData.json'));
+const summonerInfoData = JSON.parse(fs.readFileSync('./test/sampleData/summonerInfo.json'));
 
 describe('Parser', function () {
     describe('parseBody', function () {
@@ -39,11 +41,16 @@ describe('ResponseTemplates', function () {
         });
     });
 
-    // describe('Summoner', function () {
-    //     it('should generate a non-empty summoner template', function () {
-    //         // finish without errors
-    //         var response = ChampionHighscoresResponseTemplate(championHighscoreData.highscores, championTestData.champions);
-    //         assert.ok(response);
-    //     });
-    // });
+    describe('Summoner', function () {
+        it('should generate a non-empty summoner template', function () {
+            // finish without errors
+            var response = SummonerResponseTemplate(
+                summonerInfoData.summoner_info,
+                summonerInfoData.summoner_mastery,
+                summonerInfoData.summoner_mastery.mastery_data.slice(0,5),
+                championTestData.champions
+            );
+            assert.ok(response);
+        });
+    });
 });
